5562a90c362728834f1a4cf1b6386f3f66ad4c20
Benjamin Chafe <39773821+bchafe@users.noreply.github.com>
2024-07-07 20:17:32 -0300

    Fix latency, Add key event handling
    -Fix latency issues (delay node was 100% wet)
    
    -Fix gain issue when triggering envelope (context.currentTime was passed as velocity by mistake)
    
    -Add key event handling for spacebar (activates amplitude envelope)
    
    -Add controls for FM oscillator parameters

--- dubsiren/src/App.svelte
+++ dubsiren/src/App.svelte
@@ -1,11 +1,21 @@
 <script>
   import * as Tone from "tone";
 
-  let freq = 440;
+  //Tone.getContext().lookAhead = 0;
+  let isStarted = false;
+  let isSpaceDown = false;
+//osc params
+  let frequency = 440;
+  let harmonicity = 0.2;
+  let modulationIndex = 3;
  
-  let channel = new Tone.Channel({volume: -16}).toDestination();
+  let channel = new Tone.Channel({volume: -24}).toDestination();
   
-  let delay = new Tone.FeedbackDelay();
+  let delay = new Tone.FeedbackDelay({
+    delayTime: "8n",
+    feedback: 0.5,
+    wet: 0.1
+  });
 
   let env = new Tone.AmplitudeEnvelope({
     attack: 0.001,
@@ -21,28 +31,77 @@
         modulationType: "triangle",
         harmonicity: 0.2,
         modulationIndex: 3
-  }).chain(env, channel).start();
+  }).chain(env, delay, channel).start();
 
-  function updateFrequency(){
-    osc.frequency.value = osc.toFrequency(freq);
-  }
+  
 
   function triggerAttack(){
+    init();
     console.log("Trigger attack");
-    env.triggerAttack(0,1);
+    env.triggerAttack(Tone.context.currentTime, 1);
   }
 
   function triggerRelease(){
     console.log("Trigger release");
-    env.triggerRelease(0);
+    env.triggerRelease(Tone.context.currentTime);
   }
+
+  function onKeyDown(event){
+    if (event.repeat) return;
+    switch(event.key){
+      case " ":
+        isSpaceDown = true;
+        event.preventDefault();
+        triggerAttack();
+        break;
+    }
+  }
+
+  function onKeyUp(event){
+    switch(event.key){
+      case " ":
+        isSpaceDown = false;
+        event.preventDefault();
+        triggerRelease();
+        break;
+    }
+  }
+
+  function init(){
+    if(!isStarted){
+      Tone.start();
+      isStarted = true;
+      console.log("context started");
+    }
+  }
+
+$: if (isStarted){
+  osc.frequency.value = osc.toFrequency(frequency);
+  osc.harmonicity.value = harmonicity;
+  osc.modulationIndex.value = osc.toFrequency(modulationIndex);
+}
 
 </script>
 
 
-<div class="controls">
-  <p><label for="freq">Frequency: {freq}</label>
-    <input type="range" id="freq" min="20" max="5000" step="1" bind:value={freq} on:change={() => updateFrequency()}>
+<div class="osc-controls">
+  <h3>Osc. Controls</h3>
+  <p><label for="freq">Frequency: {frequency}</label>
+    <input type="range" id="freq" min="20" max="5000" step="1" bind:value={frequency}>
+  </p>
+  <p><label for="freq">Harmonicity {harmonicity}</label>
+    <input type="range" id="freq" min="0" max="8" step="0.1" bind:value={harmonicity}>
+  </p>
+  <p><label for="freq">Mod. Index {modulationIndex}</label>
+    <input type="range" id="freq" min="0" max="20" step="0.1" bind:value={modulationIndex}>
   </p>
   <button on:mousedown={() => triggerAttack()} on:mouseup={() => triggerRelease()} >fire</button>
 </div>
+
+<svelte:window 
+on:keydown={onKeyDown} 
+on:keyup={onKeyUp} />
+
+<style>
+
+</style>
